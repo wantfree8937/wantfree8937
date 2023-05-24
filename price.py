a803bd4ec6e017b1f232322312f8b73276c95ecb
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2023-05-24 16:54:49 +0900

    price.py

--- price.py
+++ price.py
@@ -1,3 +1,9 @@
+data_path = Path() / "data"
+data_path.mkdir(parents=True, exist_ok=True)
+
+code_path = Path() / "codes"
+code_path.mkdir(parents=True, exist_ok=True)
+
 def shopping(shop_file):
     shop_dict = {} # 생성할 사전 객체
 
